d278e8d056dbbd92828c6437b114b45a080ee018
ikaaaa21 <himikakaur21@gmail.com>
2026-02-19 11:19:27 +0000

    Update metadata_analysis.py

--- metadata_analysis.py
+++ metadata_analysis.py
@@ -1,8 +1,21 @@
 import pyshark
 import pandas 
 
-capture = pyshark.FileCapture(input_file='data/shortCommand_ls_01.pcap', display_filter='tcp.port == 22')
+capture = pyshark.FileCapture(
+    input_file='data/shortCommand_ls_01.pcap', 
+    display_filter='tcp.port == 22'
+)
 
 for packet in capture:
-   # do something with the packet
-    print(packet.length,packet.sniff_time, packet.ip.src, packet.ip.dst)
+    print(f'packet_length = {packet.length},'
+      f'timestamp = {packet.sniff_time},'
+      f'source_ip = {packet.ip.src},'
+      f'destination_ip = {packet.ip.dst}')
+
+client_ip = "192.168.56.102"
+server_ip = "192.168.56.103"
+
+if packet.ip.src == client_ip and packet.ip.dst == server_ip:
+    print("Packet from client to server")
+elif packet.ip.src == server_ip and packet.ip.dst == client_ip:
+    print("Packet from server to client")

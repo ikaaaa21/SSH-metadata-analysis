fa260486e6fcc527be43bbcd9fd96e76cf58133f
ikaaaa21 <himikakaur21@gmail.com>
2026-02-19 22:41:44 +0000

    fixed timestamp

--- metadata_analysis.py
+++ metadata_analysis.py
@@ -2,58 +2,61 @@ import pyshark
 import pandas 
 import csv
 
-client_ip = "192.168.56.102"
-server_ip = "192.168.56.103"
+client_ip = "192.168.56.102" # this is my client VM's IP address
+server_ip = "192.168.56.103" # this is my server VM's IP address
 
-previous_packet_time = {}
+previous_packet_time = {} # the inital previous packet 
 
+# read the pcap file and filter for TCP packets on port 22 (SSH)
 capture = pyshark.FileCapture(
     input_file='data/shortCommand_ls_01.pcap', 
     display_filter='tcp.port == 22'
 )
 
-filename = "shortCommand_ls_01.csv"
+# open a CSV file
+csv_file = "shortCommand_ls_01.csv"
 
-with open(filename, 'a', newline='') as csvfile:
+with open(csv_file, 'a', newline='') as csvfile:
    csvwriter = csv.writer(csvfile)
 
    csvwriter.writerow([
-      'packet_length', 'timestamp', 'source_ip', 
+      'stream', 'packet_length', 'timestamp', 'source_ip', 
       'destination_ip', 'time_difference', 'direction'
    ])
-
+# loop through the filtered packets and extract the required metadata
    for packet in capture:
       try:
-         if int(packet.tcp.srcport) == 22:
+
+         stream = int(packet.tcp.stream)
+         timestamp = packet.sniff_time
+         source_ip = packet.ip.src
+         destination_ip = packet.ip.dst  
+         packet_length = int(packet.tcp.len)
+
+         if int(packet.tcp.dstport) == 22:
             direction = "client to server"
 
-         elif int(packet.tcp.dstport) == 22:
+         elif int(packet.tcp.srcport) == 22:
             direction = "server to client"
 
 
-         if packet.sniff_time not in previous_packet_time:
-            time_difference = 0 
+         if stream not in previous_packet_time:
+            time_difference = 0.0
          else:
-            time_difference = (packet.sniff_time - previous_packet_time[packet.sniff_time]).total_seconds()
+            time_difference = (
+               timestamp - previous_packet_time[stream]
+            ).total_seconds()
       
-         previous_packet_time[packet.sniff_time] = packet.sniff_time
+         previous_packet_time[stream] = timestamp
 
          print(
-            f'packet_length = {packet.length},'
-            f'timestamp = {packet.sniff_time},'
-            f'source_ip = {packet.ip.src},'
-            f'destination_ip = {packet.ip.dst},'
-            f'time_difference = {time_difference},'
-            f'direction = {direction}'
+            f'stream: {stream}, tcp_len: {packet_length}, timestamp: {timestamp}, source_ip: {source_ip}, '
+            f'destination_ip: {destination_ip}, time_difference: {time_difference},'
+            f'direction: {direction}'
          )
          
          csvwriter.writerow([
-            packet.length,
-            packet.sniff_time,
-            packet.ip.src,
-            packet.ip.dst,
-            time_difference,
-            direction
+            stream, packet_length, timestamp, source_ip, destination_ip, time_difference, direction
          ])
 
       except AttributeError:

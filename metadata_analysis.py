b4e2cebfbb418e362223f0d0dcfbc534826ccd22
ikaaaa21 <himikakaur21@gmail.com>
2026-02-19 21:34:32 +0000

    made code easier to read

--- metadata_analysis.py
+++ metadata_analysis.py
@@ -1,6 +1,7 @@
 import pyshark
 import pandas 
 import csv
+
 client_ip = "192.168.56.102"
 server_ip = "192.168.56.103"
 
@@ -11,33 +12,50 @@ capture = pyshark.FileCapture(
     display_filter='tcp.port == 22'
 )
 
+filename = "shortCommand_ls_01.csv"
+
+with open(filename, 'a', newline='') as csvfile:
+   csvwriter = csv.writer(csvfile)
+
+   csvwriter.writerow([
+      'packet_length', 'timestamp', 'source_ip', 
+      'destination_ip', 'time_difference', 'direction'
+   ])
+
 for packet in capture:
-    
-   if packet.ip.src == client_ip and packet.ip.dst == server_ip:
-    direction = "client to server"
+    try:
+      if packet.ip.src == client_ip and packet.ip.dst == server_ip:
+         direction = "client to server"
 
-   elif packet.ip.src == server_ip and packet.ip.dst == client_ip:
-      direction = "server to client"
+      elif packet.ip.src == server_ip and packet.ip.dst == client_ip:
+         direction = "server to client"
 
-   if previous_packet_time is None:
-      time_difference = 0 
-   else:
-      time_difference = (packet.sniff_time - previous_packet_time).total_seconds()
+      if previous_packet_time is None:
+         time_difference = 0 
+      else:
+         time_difference = (packet.sniff_time - previous_packet_time).total_seconds()
    
-   previous_packet_time = packet.sniff_time
+      previous_packet_time = packet.sniff_time
 
-   print(f'packet_length = {packet.length},'
-      f'timestamp = {packet.sniff_time},'
-      f'source_ip = {packet.ip.src},'
-      f'destination_ip = {packet.ip.dst},'
-      f'time_difference = {time_difference},'
-      f'direction = {direction}')
-   
-   filename = "shortCommand_ls_01.csv"
-   with open(filename, 'a') as csvfile:
-      csvwriter = csv.writer(csvfile)
-      csvwriter.writerow(['packet_length', 'timestamp', 'source_ip', 'destination_ip', 'time_difference', 'direction'])
+      print(
+         f'packet_length = {packet.length},'
+         f'timestamp = {packet.sniff_time},'
+         f'source_ip = {packet.ip.src},'
+         f'destination_ip = {packet.ip.dst},'
+         f'time_difference = {time_difference},'
+         f'direction = {direction}'
+      )
+      
+      csvwriter.writerow([
+         packet.length,
+         packet.sniff_time,
+         packet.ip.src,
+         packet.ip.dst,
+         time_difference,
+         direction
+      ])
 
-      csvwriter.writerow([packet.length, packet.sniff_time, packet.ip.src, packet.ip.dst, time_difference, direction]) 
+    except AttributeError:
+        continue
 
 capture.close()

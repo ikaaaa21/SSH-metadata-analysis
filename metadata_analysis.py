c95e52e4c673edd0a291bc9f74461ddfe08aa767
ikaaaa21 <himikakaur21@gmail.com>
2026-02-19 12:58:18 +0000

    save results into a csv

--- metadata_analysis.py
+++ metadata_analysis.py
@@ -1,8 +1,10 @@
 import pyshark
 import pandas 
-
+import csv
 client_ip = "192.168.56.102"
 server_ip = "192.168.56.103"
+
+previous_packet_time = None
 
 capture = pyshark.FileCapture(
     input_file='data/shortCommand_ls_01.pcap', 
@@ -17,8 +19,8 @@ for packet in capture:
    elif packet.ip.src == server_ip and packet.ip.dst == client_ip:
       direction = "server to client"
 
-   if previous_packet_time == 0:
-      time_difference = (packet.sniff_time - 0).total_seconds()
+   if previous_packet_time is None:
+      time_difference = 0 
    else:
       time_difference = (packet.sniff_time - previous_packet_time).total_seconds()
    
@@ -30,5 +32,12 @@ for packet in capture:
       f'destination_ip = {packet.ip.dst},'
       f'time_difference = {time_difference},'
       f'direction = {direction}')
+   
+   filename = "shortCommand_ls_01.csv"
+   with open(filename, 'a') as csvfile:
+      csvwriter = csv.writer(csvfile)
+      csvwriter.writerow(['packet_length', 'timestamp', 'source_ip', 'destination_ip', 'time_difference', 'direction'])
+
+      csvwriter.writerow([packet.length, packet.sniff_time, packet.ip.src, packet.ip.dst, time_difference, direction]) 
 
 capture.close()
